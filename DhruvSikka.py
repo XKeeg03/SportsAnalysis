611ca706fe7d8960e9a9299415cacd66ea1d81e7
xke11 <xke11@uclive.ac.nz>
2025-06-13 10:16:35 +1200

    Started Following Model of paper

---
+++ DhruvSikka.py
@@ -0,0 +1,24 @@
+# Modelling of Dhruv Sikka Method for predicting win percentage
+
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.gaussian_process import GaussianProcessRegressor
+
+
+"""
+Algorithm 1: MLR Model
+Result: Prediction of win percentage of team
+"""
+lm = LinearRegression()
+
+"""
+Algorithm 2: Decision Tree Model
+Result: Prediction of win percentage of team
+"""
+
+
+"""
+Algorith 3: Random Forest Model
+Result: Prediction of win percentage of team
+"""
--- StatAcquire.py
+++ StatAcquire.py
@@ -1,8 +1,15 @@
 from nba_api.stats.static import teams
 from nba_api.stats.endpoints import leaguegamefinder
 import pandas as pd
+import time
+
+year = '2023'
 
 for team in teams.get_teams():
     id = (team['id'])
     game_finder = leaguegamefinder.LeagueGameFinder(team_id_nullable= id)
-    
+    games = game_finder.get_data_frames()
+    for game in games:
+        print(game.head(1))
+
+    time.sleep(0.4)
